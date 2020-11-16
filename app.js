c913eaefbc7f6f5907cda6d94cc24e29cbb13925
superBaik <hielelshadday@gmail.com>
2020-11-16 22:10:28 +0900

    chap2

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+import {localsMiddleware} from "./middlewares";
+import routes from "./routes";  //URL 
+
+//라우터 
+import userRouter from "./routers/userRouter" //끝이 파일이름 
+import videoRouter from "./routers/videoRouter" //끝이 파일이름 
+import globalRouter from "./routers/globalRouter" //끝이 파일이름 
+
+
+
+const app = express();
+
+
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
---
+++ routers/globalRouter.js
@@ -0,0 +1,32 @@
+import express from "express";
+import routes from "../routes";
+
+import { home, search } from "../controllers/videoController";
+import {
+    getJoin,
+    getLogin,
+    postJoin,
+    postLogin,
+    logout,
+  } from "../controllers/userController";
+
+
+const globalRouter = express.Router();
+
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+
+
+globalRouter.get(routes.logout, logout);
+
+
+
+export default globalRouter;
